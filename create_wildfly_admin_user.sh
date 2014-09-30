68ca5fa2a53ec4a3bbd298b6eee00deb0763b983
Jens Piegsa <piegsa@gmail.com>
2014-09-30 23:59:35 +0200

    Update create_wildfly_admin_user.sh

--- create_wildfly_admin_user.sh
+++ create_wildfly_admin_user.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ -f /.wildfly_admin_created ]; then
-  echo "The WildFly admin user has already been created."
+if [ -f /opt/.wildfly_admin_created ]; then
+  echo "The WildFly 'admin' user has already been created."
   exit 0
 fi
 
 #generate password
 PASS=${WILDFLY_PASS:-$(pwgen -s 12 1)}
 _type=$( [ ${WILDFLY_PASS} ] && echo "preset" || echo "random" )
-echo "=> Creating the WildFly user admin with the ${_type} password ${PASS}."
+echo "=> Creating the WildFly user 'admin' with the ${_type} password '${PASS}'."
 /opt/wildfly/bin/add-user.sh admin ${PASS} --silent
 echo "=> Done!"
-touch /.wildfly_admin_created
+touch /opt/.wildfly_admin_created
 echo "========================================================================"
 echo "You can now configure this WildFly server using:"
 echo ""
