7a9de722de40e2757ba01bf652ca20095bb6fced
Jens Piegsa <piegsa@gmail.com>
2016-08-20 15:47:03 +0200

    admin only mode flag introduced

--- run.sh
+++ run.sh
@@ -4,4 +4,9 @@ if [ ! -f /opt/.wildfly_admin_created ]; then
   /create_wildfly_admin_user.sh
 fi
 
-exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+if  [ -f /opt/wildfly/ADMIN_ONLY_MODE ]; then
+  ADMIN_ONLY_MODE_FLAG=--admin-only
+  rm /opt/wildfly/ADMIN_ONLY_MODE
+fi
+
+exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 $ADMIN_ONLY_MODE_FLAG
